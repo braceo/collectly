9bce1f5b7fb36e9b16748ed956d4a7e309790bee
Ben Brace <130545338+braceo@users.noreply.github.com>
2025-08-19 12:10:34 +0100

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
 
-export function createSupabaseBrowserClient() {
-  return createBrowserClient(
+export const createSupabaseBrowserClient = () => {
+  return createBrowserSupabaseClient()
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
