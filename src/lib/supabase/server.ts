c526af07a21de025848d768a2ecf64fecfb41d00
Ben Brace <130545338+braceo@users.noreply.github.com>
2025-08-19 12:15:55 +0100

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,8 +1,5 @@
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { type Database } from '@/types/supabase' // optional, if you use typed DB
+import { type Database } from '@/types/supabase'
 
 export const createSupabaseServerClient = () => {
   return createServerComponentClient<Database>({ cookies })
 }
-
